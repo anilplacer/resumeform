f1de09e49db8bb8825148a6dee6f67dda1fa5b85
anilplacer <72486037+anilplacer@users.noreply.github.com>
2020-10-07 13:09:07 +0530

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,18 @@
 <?php
-
-	$mail = new PHPMailer(true); 
 	if(isset($_POST['submit'])){
 		$fname=$_POST['fname'];
 		$lname=$_POST['lname'];
 		$email=$_POST['email'];
 		$phone=$_POST['phone'];
-		$objective=$_POST['objective'];
+		$message=$_POST['message'];
 
-		$to='anilplacer@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$to='anilplacer@mail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Resume Form';
-		$message="First Name :".$fname."\n"."Last Name :".$lname."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$email;
+		$message="First Name :".$fname."\n"."Last Name :".$lname."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$message;
+		$header="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$fname.", We will contact you shortly!</h1>";
+		if(mail($to, $subject, $message, $header)){
+			echo "Sent Successfully";
 		}
 		else{
 			echo "Something went wrong!";
